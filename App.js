00acefc8a977fa3c3974ebd4a6ac0467cbecad8a
alejogonza <alejo.1996.2001@gmail.com>
2020-06-15 16:56:54 -0500

    [feature - model] modelo base

--- App.js
+++ App.js
@@ -1,140 +1,116 @@
-// IMPORTS
-import React, { useState, useEffect, createContext, useReducer } from "react";
-import { View, Platform, ActivityIndicator } from "react-native";
+import React, { Component } from "react";
+
+import { Text, Image, Button, Alert, TextInput, View } from "react-native";
+
+import { Platform } from "react-native";
 import styled from "styled-components/native";
-import AsyncStorage from "@react-native-community/async-storage";
-
-// Navigation
-// navigator.push("screenName") || navigation.navigate("screenName") Adds another screen into the stack.
-// navigator.pop() || navigation.goBack() remove one screen from the stack. In other words goes back to previous screen.
-// navigation.popToTop() goes back to first screen in stack
-
-// Passing data to routes
-// navigation.navigate('RouteName', { /* params go here */ }) // pass data as params (props) between screens. (props.route): route.params
-// Note: navigation.navigate("routeName", {}) if the screen is below, it will work as goBack() function and you can send data back
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-// Screens
-import HomeScreen from "./screens/authenticated/homeScreen";
-import SignInScreen from "./screens/not_authenticated/signInScreen";
-import SignUpScreen from "./screens/not_authenticated/signUpScreen";
-import ResetPasswordScreen from "./screens/not_authenticated/resetPasswordScreen";
-
-import { AuthContext } from "./components/context";
-
-const Stack = createStackNavigator();
-function fetchAuthFromServer() {}
-
-export default function App() {
-  // User auth fetched from server (Simulation)
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [userToken, setUserToken] = useState(null); //"AYjyMzY3ZDhiNmJkNTY";
-  // const isSignOut = false;
-
-  const initialLoginState = {
-    isLoading: true,
-    userName: null,
-    userToken: null,
+import { AntDesign } from "@expo/vector-icons";
+class contractorApp extends Component {
+  SignIn = () => {
+    Alert.alert("AQUI VA AXIOS");
   };
+  
 
-  const loginReducer = (prevState, action) => {
-    switch (action.type) {
-      case "RETRIEVE_TOKEN":
-        return {
-          ...prevState,
-          userToken: action.token,
-          isLoading: false,
-        };
-      case "LOGIN":
-        return {
-          ...prevState,
-          userName: action.id,
-          userToken: action.token,
-          isLoading: false,
-        };
-      case "LOGOUT":
-        return {
-          ...prevState,
-          userName: null,
-          userToken: null,
-          isLoading: false,
-        };
-      case "REGISTER":
-        return {
-          ...prevState,
-          userName: action.id,
-          userToken: action.token,
-          isLoading: false,
-        };
-    }
-  };
-
-  const [loginState, dispatch] = useReducer(loginReducer, initialLoginState);
-
-  const authContext = React.useMemo(
-    () => ({
-      signIn: async (userName, password) => {
-        // Fetch from Server API
-        let userToken = null;
-        if (userName == "user" && password == "pass") {
-          userToken = "AYjyMzY3ZDhiNmJkNTY";
-          try {
-            await AsyncStorage.setItem("userToken", userToken);
-          } catch (e) {
-            console.log(e);
-          }
-        }
-        dispatch({ type: "LOGIN" });
-      },
-      signOut: async () => {
-        // setUserToken(null);
-        // setIsLoading(false);
-        console.log("signed Out");
-        dispatch({ type: "LOGOUT", id: userName, token: userToken });
-      },
-      signUp: () => {
-        // Set user token
-        setUserToken("AYjyMzY3ZDhiNmJkNTY");
-        setIsLoading(false);
-        console.log("signing Up");
-      },
-    }),
-    []
-  );
-
-  useEffect(() => {
-    // Get TOKEN from AsyncStorage
-    setTimeout(async () => {
-      let userToken = null;
-      try {
-        userToken = await AsyncStorage.getItem("userToken");
-      } catch (e) {
-        console.log(e);
-      }
-    }, 1000);
-  }, []);
-
-  if (loginState.isLoading) {
+  render() {
+    let secondTextInput;
+    let thirdTextInput;
     return (
-      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-        <ActivityIndicator size="large" />
-      </View>
+      <Container>
+      <ContainerTop>
+        <AntDesign
+          name="arrowleft"
+          size={30}
+          color="black"
+          onPress={() => navigation.goBack()}
+        />
+      </ContainerTop>
+      <ContainerTopMiddle>
+        <TextStyled>Please enter your phone number</TextStyled>
+        <ContainerMiddle>
+          <TextInputStyled
+            maxLength={3}
+            underlineColorAndroid="transparent"
+            keyboardType={"numeric"}
+            onChangeText={(e) => e.length == 3 && secondTextInput.focus()}
+            blurOnSubmit={false}
+          />
+          <TextInputStyled
+            ref={(input) => {
+              secondTextInput = input;
+            }}
+            maxLength={3}
+            underlineColorAndroid="transparent"
+            keyboardType={"numeric"}
+            focusable={true}
+            onChangeText={(e) => e.length == 3 && thirdTextInput.focus()}
+          />
+          <TextInputStyled
+            ref={(input) => {
+              thirdTextInput = input;
+            }}
+            maxLength={4}
+            underlineColorAndroid="transparent"
+            keyboardType={"numeric"}
+            onSubmitEditing={() => {
+              navigation.navigate("signInScreenConfirmation");
+            }}
+          />
+        </ContainerMiddle>
+        <ButtonStyled>
+          <TextStyledBtn style={{ color: "white" }}>Continue</TextStyledBtn>
+        </ButtonStyled>
+      </ContainerTopMiddle>
+    </Container>
     );
   }
-
-  return (
-    <AuthContext.Provider value={authContext}>
-      <NavigationContainer>
-        {loginState.userToken === null ? (
-          <Stack.Navigator headerMode="none">
-            <Stack.Screen name="Home" component={SignInScreen} />
-          </Stack.Navigator>
-        ) : (
-          <HomeScreen />
-        )}
-      </NavigationContainer>
-    </AuthContext.Provider>
-  );
 }
+const TextInputStyled = styled.TextInput`
+  margin: 10px;
+  border: black;
+  border-radius: 10px;
+  text-align: center;
+  height: 50px;
+  width: 70px;
+  font-size: ${() => (Platform.OS == "ios" ? "25px" : "17px")};
+`;
+
+const ButtonStyled = styled.TouchableOpacity`
+  background-color: #548ff7;
+  padding: ${() => (Platform.OS == "ios" ? "15px" : "10px")};
+  width: 80%;
+  border-radius: 6px;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.View`
+  flex: 1;
+`;
+const TextStyledBtn = styled.Text`
+  font-size: ${() => (Platform.OS == "ios" ? "25px" : "17px")};
+`;
+
+const ContainerMiddle = styled.View`
+  align-items: center;
+  margin: 50px;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+const ContainerTop = styled.View`
+  margin-top: 70px;
+  margin-left: 30px;
+`;
+const ContainerTopMiddle = styled.View`
+  flex: 1;
+  padding: 20px;
+  padding-left: 20px;
+`;
+
+const TextStyled = styled.Text`
+  margin: 20px 0;
+  font-size: 31px;
+`;
+
+export default contractorApp;
