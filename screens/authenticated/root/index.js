2dd51415e089d0169e408b3b7100acb871ff5d21
Bryan Enid <30734480+BryanEnid@users.noreply.github.com>
2020-06-16 10:33:24 -0400

    [Fix] All broken changes

--- screens/authenticated/root/index.js
+++ screens/authenticated/root/index.js
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
+import * as Location from "expo-location";
+import styled from "styled-components/native";
+
+// Interfaces
+import { CameraInterface } from "../../../interfaces/mapview-interfaces";
+
+// Expo
+import { MaterialIcons } from "@expo/vector-icons";
+import MapView, { Marker } from "react-native-maps";
 
 export function RootScreen({ navigation }) {
   const [location, setLocation] = useState(null);
@@ -123,3 +132,79 @@ export function RootScreen({ navigation }) {
     return <View></View>;
   }
 }
+
+// STYLES
+const Container = styled.View`
+  flex: 1;
+`;
+
+const Menu = styled.TouchableOpacity`
+  position: absolute;
+  left: 8%;
+  top: 6%;
+  border-radius: 50px;
+  background: white;
+  padding: 10px;
+`;
+
+const Card = styled.View`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  border-radius: 40px;
+  background: white;
+  width: 100%;
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  justify-content: ${({ first, last }) => {
+    switch (true) {
+      case first:
+        return "space-between";
+      case last:
+        return "space-around";
+      default:
+        return "flex-start";
+    }
+  }};
+  /* justify-content:  */
+  margin: ${(props) => {
+    if (props.first) {
+      return "30px 10px 0 10px";
+    } else if (props.last) {
+      return `20px 10px 50px 10px`;
+    } else {
+      return "0px 0px";
+    }
+  }};
+  padding: 0 30px;
+  border-bottom-color: #eaeaea;
+  border-bottom-width: ${(props) => (props.last ? "0px" : "1px")};
+`;
+
+const Text = styled.Text`
+  margin: 5px 0;
+  ${({ title, medium, small }) => {
+    switch (true) {
+      case title:
+        return `font-size: 22px`;
+
+      case medium:
+        return `font-size: 20px`;
+
+      case small:
+        return `font-size: 39px`;
+    }
+  }}
+
+  ${({ bold, light }) => {
+    switch (true) {
+      case bold:
+        return `font-weight: 800`;
+
+      case light:
+        return `font-weight: 300; color: #999;`;
+    }
+  }}
+`;
