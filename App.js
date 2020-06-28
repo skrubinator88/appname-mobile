d9bff51e60d8d50cf7ce3ca9caa997ea4b81cd3e
alejogonza <alejo.1996.2001@gmail.com>
2020-06-28 00:21:08 -0500

    [feature] add 47 view

--- App.js
+++ App.js
@@ -1,219 +1,171 @@
 import React, { Component } from "react";
 
-import {
-  Text,
-  CheckBox,
-  Alert,
-  TouchableWithoutFeedback,
-  TextInput,
-  View,
-  Keyboard,
-  ScrollView,
-  FlatList,
-} from "react-native";
-
-import {
-  TextField,
-  FilledTextField,
-  OutlinedTextField,
-} from "react-native-material-textfield";
-
-import { Platform } from "react-native";
+import { View, StyleSheet, Image, Text } from "react-native";
 import styled from "styled-components/native";
-import { AntDesign } from "@expo/vector-icons";
-import StarRating from "react-native-star-rating";
+import { Ionicons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+
 class contractorApp extends Component {
-  state = {
-    search: "",
-    DATA: [
-      {
-        id: "1",
-        image: "./assets/cheems2.jpg",
-        title: "Great Task Management",
-      },
-      {
-        id: "2",
-        image: "./assets/cheems2.jpg",
-        title: "Excellent Service",
-      },
-      {
-        id: "3",
-        image: "./assets/cheems2.jpg",
-        title: "Excellent Negotiator",
-      },
-      {
-        id: "4",
-        image: "./assets/cheems2.jpg",
-        title: "Great Task Management",
-      },
-      {
-        id: "5",
-        image: "./assets/cheems2.jpg",
-        title: "Great Task Management",
-      },
-      {
-        id: "6",
-        image: "./assets/cheems2.jpg",
-        title: "Great Task Management",
-      },
-    ],
-  };
-
-
-  updateSearch = (search) => {
-    this.setState({ search });
-    console.log(search)
-  };
-
-  onStarRatingPress(rating) {
-    this.setState({
-      starCount: rating,
-    });
-  }
   render() {
-    const { search } = this.state;
-    const Cancel = (e) => {
-      Alert.alert("Cancel");
-    };
-    const Save = (e) => {
-      Alert.alert("Save");
-    };
-
-    function Item({ title, image }) {
-      return (
-      <Margin>
-          <LicResult>
-           <CompImage source={require("./assets/cheems2.jpg")} />
-          <TextResult style={{ color: "black", width:120 }}>{title}</TextResult>
-        </LicResult>
-      </Margin>
-      );
-    }
-
     return (
-      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        
-        <Container>
-        <ContainerTop>{/* aqui va el header */}</ContainerTop>
-        <Fields>
-        <UserImage source={require("./assets/cheems2.jpg")} />
-            <TextStyledContent>Jhon Doe</TextStyledContent>
-            <TextResult>Company Co. LLC</TextResult>
-            <RateBox>
-              <TextResult style={{ color: "black" }}>Good</TextResult>
-              <StarRating
-                disabled={false}
-                maxStars={5}
-                rating={this.state.starCount}
-                selectedStar={(rating) => this.onStarRatingPress(rating)}
-              />
-         
-            </RateBox>
-          <ResultBox>
-         
-          <TextResult style={{ color: "black", marginTop:10,marginBottom:10 }}>Give a compliment</TextResult>
-          
-            <FlatList
-              horizontal={true}
-              data={this.state.DATA}
-              renderItem={({ item }) => (
-                <Item
-                  title={item.title}
-                  image={item.image}
-                />
-              )}
-              keyExtractor={(item) => item.id}
+      <Container>
+        <HeaderBox>
+          <IconLeft>
+            <Ionicons
+              name="md-trash"
+              size={25}
+              color="#6A6A6A"
+              onPress={() => navigation.goBack()}
             />
+          </IconLeft>
 
-          </ResultBox>          
-        </Fields>
-        <BtnContainer>
-        <TextField style={{margin: 100}} label="Leave a comment" />
-        </BtnContainer>
-          
-        </Container>
-      </TouchableWithoutFeedback>
+          <IconRight>
+            <Entypo
+              name="star"
+              size={25}
+              color="#34C5EB"
+              onPress={() => navigation.goBack()}
+            />
+          </IconRight>
+        </HeaderBox>
+
+        <ContentBox>
+          <ImageProfile source={require("./assets/Ellipse_1.png")} />
+   <ContentColum>
+   <ContentInfo>
+            <StyledText style={{ fontWeight: "bold" }}>_domakeup</StyledText>
+            <StyledText>Daniela Ordoñez</StyledText>
+          </ContentInfo>
+          <ContentFollow>
+            <ContentData>
+               <StyledText style={{ fontWeight: "bold" }}>100k</StyledText>
+          <StyledText>Seguidores</StyledText>
+            </ContentData>
+            <ContentData>
+                  <StyledText style={{ fontWeight: "bold" }}>9.9</StyledText>
+         <StyledText>Rating</StyledText>
+            </ContentData>
+        
+
+          </ContentFollow>
+   </ContentColum>
+        </ContentBox>
+        <ButtonBox>
+          <FollowBtn>Seguir</FollowBtn>
+          <ShareBtn>Compartir</ShareBtn>
+        </ButtonBox>
+      </Container>
     );
   }
 }
 
-const UserImage = styled.Image`
-  width: 80px;
-  height: 80px;
-  border-radius: 50px;
-  margin-top: 20px;
-`;
-
-const CompImage = styled.Image`
-  width: 80px;
-  height: 80px;
-  border-radius: 50px;
-  margin-top: 10px;
-`;
-const Fields = styled.View`
-  justify-content: center;
-  align-items: center;
-  margin: 20px;
-  margin-bottom:0px;
-`;
-const LicResult = styled.View`
-  padding-top: 15px;
-  padding-bottom: 15px;
-  align-items:center;
-
-`;
-
-const RateBox = styled.View`
-  margin: 20px;
-  padding: 10px;
-  margin-top: 5px;
-`;
-
 const Container = styled.View`
-  flex: 1;
-  flex-direction:column;
-
-`;
-const BtnContainer = styled.View`
-  flex: 1;
- 
-`;
-const Margin = styled.View`
-  margin-left:50px;
-  margin-right:50px;
-
-`;
-
-const ContainerTop = styled.View`
-  flex: 0.7;
-  background: #3869f3;
-`;
-
-const ResultBox = styled.View`
-  margin: 20px;
-  margin-top: 1px;
-  justify-content: center;
-  align-items: center;
+  flex: 0.4;
+  flex-direction: column;
+  margin-top: 35px;
+  justify-content: flex-end;
+  margin-left: 10px;
+  margin-right: 10px;
+  /* background:red;  */
   border-width: 1px;
+  border-color: #e6e6e6;
+
+  border-radius: 10px;
+`;
+
+const IconLeft = styled.View`
+  justify-content: flex-start;
+  flex-direction: row;
+  margin-right: 10px;
+  margin-left: 20px;
+  margin-top: 10px;
+  margin-bottom: 8px;
+`;
+
+const IconRight = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  flex-direction: row;
+  margin-right: 20px;
+  margin-left: 10px;
+  margin-top: 10px;
+  margin-bottom: 8px;
+`;
+
+const ButtonBox = styled.View`
+  /* background:yellow; */
+  flex-direction: row;
+`;
+
+const ContentBox = styled.View`
+  flex: 1.4;
+
+  /* background: red; */
+  flex-direction: row;
+`;
+
+const HeaderBox = styled.View`
+  flex: 0.4;
+  /* background: yellow; */
+  flex-direction: row;
+`;
+
+const FollowBtn = styled.Text`
+  padding: 20px;
+  width: 50%;
+  text-align: center;
+  border-width: 1px;
+  border-bottom-width: 0px;
+  border-left-width: 0px;
+  border-color: #e6e6e6;
+  font-size: 18px;
+`;
+
+const ShareBtn = styled.Text`
+  padding: 20px;
+  width: 50%;
+  text-align: center;
+  border-width: 1px;
+  border-bottom-width: 0px;
   border-left-width: 0px;
   border-right-width: 0px;
-  border-bottom-width: 0px;
-  width: 500px;
-  border-color: #efefef;
+  border-color: #e6e6e6;
+  font-size: 18px;
 `;
 
-const TextStyledContent = styled.Text`
-  text-align: center;
-  margin-top: 10px;
-  font-weight: bold;
-  font-size: ${() => (Platform.OS == "ios" ? "25px" : "20px")};
+const ImageProfile = styled.Image`
+  width: 135px;
+  height: 135px;
+  margin-left: 16px;
+  margin-right: 5px;
 `;
 
-const TextResult = styled.Text`
-  color: #a5a5a5;
+const StyledText = styled.Text`
   text-align: center;
-  margin-bottom: 10px;
-  font-size: ${() => (Platform.OS == "ios" ? "25px" : "18px")};
+  margin: 5px;
+  font-size:18px;
+`;
+
+const ContentInfo = styled.View`
+  flex: 0.5;
+  /* background: green; */
+`;
+
+const ContentData = styled.View`
+  flex: 1;
+  /* background: blueviolet; */
+`;
+
+const ContentFollow = styled.View`
+ flex:0.5;
+ flex-direction:row;
+  /* background: purple; */
+`;
+
+const ContentColum = styled.View`
+flex:1;
+flex-direction:column;
+  /* background: purple; */
 `;
 
 export default contractorApp;
