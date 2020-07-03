8da23da07bcf1160e66e592a4d9759dc7e4a158e
BuildTools <unconfigured@null.spigotmc.org>
2020-07-03 14:39:27 -0400

    [Update] History Shadows

--- views/test.js
+++ views/test.js
@@ -49,26 +49,27 @@ export default function Screen60({ navigation }) {
 
 const Container = styled.View`
   flex: 1;
-  background: blue;
+  background: #ececec;
 `;
 
-const JobHistorySection = styled.View`
-  background: red;
-`;
+const JobHistorySection = styled.View``;
 
 const Row = styled.View`
   width: 100%;
-  height: 60px;
-  background: yellow;
+  height: 63px;
+  background: white;
   padding: 10px;
-  margin-bottom: 5px;
+  margin-bottom: 8px;
   border-bottom-right-radius: 7px;
   border-bottom-left-radius: 7px;
+  border-top-right-radius: 2px;
+  border-top-left-radius: 2px;
   flex-direction: column;
+  box-shadow: 0px 2px 2px #888888;
 `;
 
 const TextBox = styled.View`
-  border: 1px solid;
+  margin: 1px;
   flex-direction: row;
   justify-content: space-between;
 `;
